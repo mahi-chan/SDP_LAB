314f71c2545b48f955ead54838e4833acc50cc50
mahi-chan <mahi2113@cseku.ac.bd>
2024-04-29 21:24:54 +0600

    fixed some code smell

--- Functions.py
+++ Functions.py
@@ -28,27 +28,41 @@ class VideoPlayer(VideoPlayer, Playlist.Playlist):
     def set_speed(self, speed):
         self.player.set_rate(float(speed))
 
-    def update_seek_slider(self):
-        video_length = self.player.get_length() / 1000
-        video_time = self.player.get_time() / 1000
-        self.seek_var.set(video_time / video_length * 100)
-        self.master.after(1000, self.update_seek_slider)
+    MILLISECONDS_TO_SECONDS = 1000
 
-    def seek_video(self, value):
-        video_length = self.player.get_length() / 1000
-        seek_time = float(value) * video_length / 100
-        self.player.set_time(int(seek_time * 1000))
+    def convert_to_seconds(milliseconds, MILLISECONDS_TO_SECONDS=None):
+        return milliseconds / MILLISECONDS_TO_SECONDS
+
+    def update_seek_slider(self, MILLISECONDS_TO_SECONDS=None):
+        video_length_seconds = self.convert_to_seconds(self.player.get_length())
+        video_time_seconds = self.convert_to_seconds(self.player.get_time())
+        seek_position_percentage = (video_time_seconds / video_length_seconds) * 100
+        self.seek_var.set(seek_position_percentage)
+        self.master.after(MILLISECONDS_TO_SECONDS, self.update_seek_slider)
+
+    def calculate_seek_time(self, percentage):
+
+        video_length_ms = self.player.get_length()  # Video length in milliseconds
+        seek_time_ms = int((percentage / 100) * video_length_ms)
+        return seek_time_ms
+
+    def seek_video(self, percentage):
+
+        seek_time_ms = self.calculate_seek_time(percentage)
+        self.player.set_time(seek_time_ms)
+
+    def get_aspect_ratio_value(self, aspect_ratio):
+        aspect_ratios = {
+            "16:9": "16:9",
+            "4:3": "4:3",
+            "1:1": "1:1"
+        }
+        return aspect_ratios.get(aspect_ratio)
 
     def set_aspect_ratio(self):
-        aspect_ratio = self.aspect_ratio_var.get()
-        if aspect_ratio == "16:9":
-            self.player.video_set_aspect_ratio("16:9")
-        elif aspect_ratio == "4:3":
-            self.player.video_set_aspect_ratio("4:3")
-        elif aspect_ratio == "1:1":
-            self.player.video_set_aspect_ratio("1:1")
-        else:
-            self.player.video_set_aspect_ratio(None)
+        selected_aspect_ratio = self.aspect_ratio_var.get()
+        aspect_ratio_value = self.get_aspect_ratio_value(selected_aspect_ratio)
+        self.player.video_set_aspect_ratio(aspect_ratio_value or None)
 
     def set_media_and_play(self, media):
         self.player.set_media(media)
